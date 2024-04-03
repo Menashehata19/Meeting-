c4f5ef12a85282d64b8f32fb85f6321960a7d29c
Menashehata19 <161930847+Menashehata19@users.noreply.github.com>
2024-04-03 12:36:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -131,3 +131,89 @@ function toggleDarkMode() {
     document.getElementById("button80").innerText = "الوضع المظلم";
   }
 }
+
+
+let meninfo = document.getElementById('infomobile');
+
+
+function btninfo(){
+  // Tab to edit
+  if (meninfo.style.display == "none") {
+    meninfo.style.display="block"
+  }
+  
+  else {
+    meninfo.style.display="none";
+  }
+  
+
+} 
+
+function exitho() {
+  // Tab to 
+  if (meninfo.style.display == "block") {
+    meninfo.style.display="none"
+  }
+  
+}
+let p1m = document.getElementById('pmenu1');
+
+let p2m = document.getElementById('pmenu2');
+
+let p3m = document.getElementById('pmenu3');
+
+
+p1m.innerHTML= "screen width : " +screen.width;
+
+
+p2m.innerHTML="ip phone " ;
+
+
+
+
+function text(url) {
+  return fetch(url).then(res => res.text());
+}
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  
+  
+  p2m.innerHTML="ip phone : " + (ip) ;
+});
+
+
+
+
+
+
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
+
+console.log;
+
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+
+
+
+p3m.innerHTML = "your use : " + (getDeviceType());
+
+
+
